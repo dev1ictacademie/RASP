6ffa6e70c0b30c9bf62f49861bedfc4e724daf55
devadv <ansemsdevries@gmail.com>
2018-03-28 16:04:09 +0200

    try  to solve the FIXME and added listener for the properties

--- src/nl/pameijer/ictacademie/rasp/view/month/splitpane/Controller.java
+++ src/nl/pameijer/ictacademie/rasp/view/month/splitpane/Controller.java
@@ -3,6 +3,8 @@ package nl.pameijer.ictacademie.rasp.view.month.splitpane;
 import java.time.LocalDate;
 
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import nl.pameijer.ictacademie.rasp.model.Model;
 
 /**
@@ -32,8 +34,11 @@ public class Controller  {
 		view = new MonthInputView(dateModel);
 		//FIXME sometimes loses the connection/bind maybe garbage collected
 		yearProperty.bind(dateModel.yearProperty());
+		MyChangeListener myChangeListener = new MyChangeListener();
+		yearProperty.addListener(myChangeListener);
 		monthProperty.bind(dateModel.monthProperty());
-		setListeners();
+		monthProperty.addListener(myChangeListener);
+		//setListeners();
 		setStudents();
 		setDayTextFields();
 	}
@@ -67,5 +72,16 @@ public class Controller  {
 	public void setDayTextFields() {
 		view.fillDayParts(model.getStudentList(), view.getGridDays());
 	}
+	
+	private class MyChangeListener implements ChangeListener
+	{
 
+		@Override
+		public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+			System.out.println(" In controller " + newValue);
+			view.clearRefreshHeader();
+			setStudents();
+			setDayTextFields();
+		}
+	}
 }
