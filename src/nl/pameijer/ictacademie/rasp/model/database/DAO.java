e241bb0df2d2ae2253b6141ba12ce0d8edde339e
dev1ictacademie <dev1.ictacademie@gmail.com>
2018-12-03 15:59:05 +0100

    no changes

--- src/nl/pameijer/ictacademie/rasp/model/database/DAO.java
+++ src/nl/pameijer/ictacademie/rasp/model/database/DAO.java
@@ -2,9 +2,9 @@ package nl.pameijer.ictacademie.rasp.model.database;
 
 public interface DAO {
 
-	public static final String DB_URL = "jdbc:mysql://localhost:3306/RASP";
+	public static final String DB_URL = "jdbc:mysql://sql.zz/wahlers_RASP";
 	public static final String DRIVER = "com.mysql.jdbc.Driver";
 	public static final String USER = "RASP";
-	public static final String PASS = "#RASP123";
+	public static final String PASS = "RASP";
 
 }
