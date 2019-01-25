9cdc5e0296dfc181d72357cb1b010e3742ffad3a
dev1ictacademie <dev1.ictacademie@gmail.com>
2019-01-25 14:03:37 +0100

    main commit

--- src/nl/pameijer/ictacademie/rasp/view/start/Main.java
+++ src/nl/pameijer/ictacademie/rasp/view/start/Main.java
@@ -19,6 +19,7 @@ public class Main extends Application {
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			
+			// close application
 			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
 				
 				@Override
